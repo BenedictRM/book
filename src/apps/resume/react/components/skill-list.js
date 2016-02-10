1e4987f0a341891908c5d4b1f107c7e8c6f724e3
BenedictRM <mehringr@gmail.com>
2016-02-10 15:44:54 -0700

    update to personal code
    here, keeps getting overwritten

--- src/apps/resume/react/components/skill-list.js
+++ src/apps/resume/react/components/skill-list.js
@@ -1,61 +1,33 @@
 MyComponents.Skill = React.createClass({
 
   render: function() {
+	var skillType = this.props.skill.key;  
+	var skillList = this.props.skill.value;  
     return (
-      <li className="collection-item">{this.props.skill}</li>
+      <div className="card">
+        <div className="card-content">
+		<p><b><font size="6"> {skillType}</font></b></p>
+  <p><b><font size="4" color = "Orange"> {skillList}</font></b></p>
+        </div>
+      </div>
     );
   }
+
 });
 
 MyComponents.SkillList = React.createClass({
   render: function() {
 
-    var languages = this.props.skills.languages.map(function(s,i){
+    var skillElements = this.props.skills.map(function(s,i){
       return <MyComponents.Skill skill={s} key={i}/>
-    });
-    var frameworks = this.props.skills.frameworks.map(function(s,i){
-      return <MyComponents.Skill skill={s} key={i}/>
-    });
-    var skills = this.props.skills.skills.map(function(s,i){
-      return <MyComponents.Skill skill={s} key={i}/>
-    });
+    })
 
     return (
-      <div className="card blue-grey lighten-1">
-        <div className="row">
-      
-          <div className="col s4">
-            <div className="card blue-grey darken-2">
-              <div className="card-content white-text">
-                <span className="card-title">Languages</span>
-                <ul className="collection black-text">
-                {languages}
-                </ul>
-              </div>
-            </div>
-          </div>
-      
-          <div className="col s4">
-            <div className="card blue-grey darken-2">
-              <div className="card-content white-text">
-                <span className="card-title">Frameworks</span>
-                <ul className="collection black-text">
-                {frameworks}
-                </ul>
-              </div>
-            </div>
-          </div>
-      
-          <div className="col s4">
-            <div className="card blue-grey darken-2">
-              <div className="card-content white-text">
-                <span className="card-title">Skills</span>
-                  <ul className="collection black-text">
-                  {skills}
-                  </ul>
-              </div>
-            </div>
-          </div>
+      <div className="card">
+        <div className="card-content">
+
+        {skillElements}
+
         </div>
       </div>
     );
